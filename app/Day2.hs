39498bd7fefc855c25dc023d6754e277e3aede4d
sarah <sarah.deitke@gmail.com>
2023-12-06 23:03:25 -0800

    day 2 part 2

--- app/Day2.hs
+++ app/Day2.hs
@@ -1,6 +1,7 @@
 module Day2 where
 import System.IO
 import Data.List.Split
+import Data.List (intercalate)
 import Data.Bool
 
 day2Funky :: IO()
@@ -14,18 +15,37 @@ day2Funky = do
 computeValidGame :: String -> Int
 computeValidGame game =
     let removePrefix = drop 5 game in -- pulling out "Game "
-    let gameNumberSplit = splitOn ":" removePrefix in
+    let gameNumberSplit = splitOn ": " removePrefix in
 
     let gameNumber = read (head gameNumberSplit) :: Int in
     let gameSetsStr = last gameNumberSplit in
 
-    let gameSets = splitOn ";" gameSetsStr in
-    let validGameArr = map computeValidSet gameSets in
+    -- Part 2
+    let gameSets = replaceSubstring ";" "," gameSetsStr in
+    let setsArr = splitOn ", " gameSets in
+    computePowerGame setsArr 0 0 0
 
-    let isGameInvalid = elem False validGameArr in
+    -- Part 1
+    -- let gameSets = splitOn ";" gameSetsStr in
+    -- let validGameArr = map computeValidSet gameSets in
 
-    if (isGameInvalid) then 0
-    else gameNumber
+    -- let isGameInvalid = elem False validGameArr in
+
+    -- if (isGameInvalid) then 0
+    -- else gameNumber
+
+computePowerGame :: [String] -> Int -> Int -> Int -> Int
+computePowerGame [] maxR maxG maxB = maxR*maxG*maxB
+computePowerGame (h:t) maxR maxG maxB =
+    let valArr = splitOn " " h in
+    let diceNum = read (head valArr) :: Int in
+    let diceColor = last valArr in
+    
+    if (diceColor == "red" && diceNum > maxR) then computePowerGame t diceNum maxG maxB
+    else if (diceColor == "green" && diceNum > maxG) then computePowerGame t maxR diceNum maxB
+    else if (diceColor == "blue" && diceNum > maxB) then computePowerGame t maxR maxG diceNum
+    else computePowerGame t maxR maxG maxB
+
 
 -- input is 2 red, 2 green
 computeValidSet :: String -> Bool
@@ -51,3 +71,8 @@ computeValidDice die =
     else if (color == "green" && numDie > 13) then False
     else if (color == "blue" && numDie > 14) then False
     else True
+
+-- Replace occurrences of a substring in a string
+replaceSubstring :: String -> String -> String -> String
+replaceSubstring old new str =
+    intercalate new $ splitOn old str
